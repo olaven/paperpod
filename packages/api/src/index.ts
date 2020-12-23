045aa687ad9890b5859f9e0deb859c58e2cd21aa
olaven <olav@sundfoer.com>
2020-12-23 12:10:06 +0100

    first commit

---
+++ packages/api/src/index.ts
@@ -0,0 +1,20 @@
+import { CREATED, OK } from "node-kall";
+import * as express from "express";
+const app = express()
+
+app.post("/articles", (request, response) => {
+
+    response.status(CREATED).send("Created");
+});
+
+
+app.get("/articles", (request, response) => {
+
+    response.status(OK).send("REtrieved");
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+
+    console.log("API is listening");
+});

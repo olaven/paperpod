a373dac82d1eea6ce5d942c115d31700542f4fa0
olaven <olav@sundfoer.com>
2021-01-04 14:23:15 +0100

    WIP: debugging gateway JSON body not being forwarded

--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -1,18 +1,41 @@
+import querystring from "querystring"
+import bodyParser from "body-parser"
 import { createProxyMiddleware } from "http-proxy-middleware";
 import { boot } from "common/src/server/boot";
 
-
+const { API_PORT, AUTHENTICATION_PORT, WEB_PORT } = process.env;
 boot("", (gateway) => {
 
+    gateway.use(bodyParser.json())
 
-    const forward = (path: string, target: string) =>
+    const forward = (path: string, target: string) => {
+
         gateway.use(path, createProxyMiddleware({
-            target
+            target,
+            onProxyReq: (proxyReq, req, res) => {
+
+                if (!req.body || !Object.keys(req.body).length) {
+                    return;
+                }
+
+                const contentType = proxyReq.getHeader('Content-Type');
+                const writeBody = (bodyData: string) => {
+                    proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+                    proxyReq.write(bodyData);
+                };
+
+                if (contentType.toString().includes('application/json')) {
+                    writeBody(JSON.stringify(req.body));
+                }
+
+                if (contentType === 'application/x-www-form-urlencoded') {
+                    writeBody(querystring.stringify(req.body));
+                }
+            }
         }));
+    }
 
-
-    const { API_PORT, AUTHENTICATION_PORT, WEB_PORT } = process.env;
-
+    forward("/proxy", 'www.google.com');
     forward("/api", "http://api:" + API_PORT);
     forward("/authentication", "http://authentication:" + AUTHENTICATION_PORT);
     forward("/", "http://web:" + WEB_PORT);

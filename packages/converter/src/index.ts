51e97f3ff6cb56c8ba1a6cf7a863c736ea13f0e0
olaven <olav@sundfoer.com>
2021-01-05 20:23:20 +0100

    uploading to subfolder -> TODO: fix user id

--- packages/converter/src/index.ts
+++ packages/converter/src/index.ts
@@ -15,7 +15,8 @@ dotenv.config();
 export const convert = async (text: string) => {
 
     const audio = await textToAudio(text);
-    const filename = upload(audio);
+
+    const filename = upload(audio, "instapod-articles", `testuserid/articleURL`);
     console.log("UPLOADED: ", filename);
 }
 
@@ -27,5 +28,4 @@ export const convert = async (text: string) => {
         Han smilte sannsynligvis like mye av det første som det siste. Det å bryte ut av rekkene og offentlig mene man fortjener en nominasjon til Idrettsgallaen mer enn andre tilsier jo at man enten har enorm selvtillit eller et særdeles dårlig taperhjerte. Eller i dette tilfellet sannsynligvis begge deler. Særlig når man vet at støyen det medfører er som pur stillhet å regne sammenlignet med hva som er i vente få dager senere.
         Forskjell på å bli satt i sjakk og i sjakk matt Magnus Carlsen liker åpenbart å være en del av folket, som er de som bestemmer hvem som blir Årets navn i norsk idrett. Så lenge folket er enige med ham. For det Carlsen gjør nå, ved å ha orkestrert avtalen mellom det svenske spillselskapet Kindred og sin egen sjakk-klubb, er å arrangere omkamp i sin strid med hele den norske idrettsmodellen.
     `);
-
 })()
--- packages/converter/src/upload.ts
+++ packages/converter/src/upload.ts
@@ -1,16 +1,16 @@
-import { Storage } from "@google-cloud/storage";
+import { Bucket, Storage } from "@google-cloud/storage";
 
+type FileName = `${string}/${string}`
 type BucketName = 'instapod-articles'
 
-const getBucket = (name: BucketName) => {
-
-    const storage = new Storage();
-    return storage.bucket(name);
-}
-
-export const upload = async (audio: Uint8Array) => {
 
 
-    const bucket = getBucket("instapod-articles");
-    await bucket.file("test-file").save(audio)
+export const upload = async (audio: Uint8Array, bucket: BucketName, filename: FileName) => {
+
+    new Storage()
+        .bucket(bucket)
+        .file(filename)
+        .save(audio);
+    /* const bucket = getBucket("instapod-articles");
+    await bucket.file("test-file").save(audio); */
 }

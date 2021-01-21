9c6d4d053740bf83a93c84f76393ec4cb9ad7fa4
Olav Sundfør <olav@sundfoer.com>
2021-01-21 12:40:22 +0100

    Scripts for installing docker machine

--- scripts/create-digitalocean-droplets.sh
+++ scripts/create-digitalocean-droplets.sh
@@ -1,8 +1,14 @@
-for i in 1 2 3; do 
-docker-machine create \
- --driver digitalocean \
- --digitalocean-region ams3 \
- --digitalocean-access-token $DOTOKEN \
- --engine-install-url https://releases.rancher.com/install-docker/19.03.9.sh \
- paperpod-node-$i \
- ; done # bug in current version https://www.digitalocean.com/community/questions/unable-to-verify-the-docker-daemon-is-listening
+function create_machine() {
+
+
+    docker-machine create \
+        --driver digitalocean \
+        --digitalocean-region ams3 \
+        --digitalocean-access-token $DOTOKEN \
+        --engine-install-url https://releases.rancher.com/install-docker/19.03.9.sh \
+        $1
+}
+
+create_machine paperpod-manager 
+create_machine paperpod-worker-1
+create_machine paperpod-worker-2
---
+++ scripts/install-docker-machine.sh
@@ -0,0 +1,2 @@
+curl -L https://github.com/docker/machine/releases/download/v0.8.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine
+sudo chmod +x /usr/local/bin/docker-machine

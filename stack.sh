541cd7fd6b586a286b7dd77d37ea24cf932395ff
Olav Sundfør <olav@sundfoer.com>
2021-01-23 16:27:56 +0100

    dockerfiles are building with typescript (should move typedefs and typescrpt to production and using --production on yarn install long term)

--- stack.sh
+++ stack.sh
@@ -3,11 +3,20 @@
 # * legge in `environment:`-entries i stack.yml
 # * eksportere variablene som trengs her  
 
+## TODO: Figure out how to pick up these variables from Github Secrets 
+
 env \
     GATEWAY_PORT=8080 \
     WEB_PORT=3000 \
     API_PORT=8081 \
     AUTHENTICATION_PORT=8082 \
+    JWT_SECRET="JWT SECRET - Generate this randomly" \
+    MONGODB_NAME=$MONGODB_NAME \ # these do not work rn, as no mongo is set
+    MONGODB_USERNAME=$MONGODB_USERNAME \ # these do not work rn, as no mongo is set
+    MONGODB_PASSWORD=$MONGODB_PASSWORD \ # these do not work rn, as no mongo is set
+    MONGODB_HOST=$MONGODB_HOST \ # these do not work rn, as no mongo is set
+    MONGODB_PORT=$MONGODB_PORT \ # 27017 \ # these do not work rn, as no mongo is set
+    GOOGLE_APPLICATION_CREDENTIALS=service-account.json \ 
     docker stack deploy -c stack.yml paperpod
 
 # export GATEWAY_PORT=8080

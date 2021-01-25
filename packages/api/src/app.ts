7f3758860811a0d4f04c4a87a9e955b90c5bc439
Olav Sundfør <olav@sundfoer.com>
2021-01-25 21:46:44 +0100

    pptr does not timeout `waitFor(*)`.

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,10 +1,23 @@
 import { server } from "@paperpod/common";
+import { withTextualData } from "@paperpod/converter";
 import { articleRoutes, fileRoutes, rssRoutes } from "./routes/routes";
 
 
 export const app = server.app.appWithBodyParser(
     server.app.appWithEnvironment()
 )
+    .use("/test", async (req, res) => {
+
+        const article = await withTextualData({
+            _id: "string",
+            owner_id: "string",
+            original_url: "https://waitbutwhy.com/2018/04/picking-career.html",
+            added_timestamp: -1,
+            storage_uri: "string",
+        })
+
+        res.send(article)
+    })
     .use(rssRoutes)
     .use(articleRoutes)
     .use(fileRoutes)

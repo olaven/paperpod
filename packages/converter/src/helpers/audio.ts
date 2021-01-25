86bb538a5c25d13429ffef992d9a2e581d9bef6e
Olav Sundfør <olav@sundfoer.com>
2021-01-25 11:14:38 +0100

    basic language detection for norwegian and english

--- packages/converter/src/helpers/audio.ts
+++ packages/converter/src/helpers/audio.ts
@@ -1,11 +1,25 @@
-import { PollyClient, SynthesizeSpeechCommand, SynthesizeSpeechOutput } from "@aws-sdk/client-polly";
+import { PollyClient, SynthesizeSpeechCommand, DescribeVoicesCommand } from "@aws-sdk/client-polly";
+import { ComprehendClient, DetectDominantLanguageCommand } from "@aws-sdk/client-comprehend";
+
 
 const getLanguage = async (text: string) => {
 
 
-    //FIXME: RETURN LANGUAGE WITH AWS COMPREHEND
-    return ""
+    const client = new ComprehendClient({ region: "eu-west-1" });
+
+    const command = new DetectDominantLanguageCommand({
+        Text: text
+    });
+    const result = await client.send(command);
+
+    const [language] = result.Languages;
+    return language.LanguageCode
 }
+
+const voiceFromLanguage = (code: string) => ({
+    "en": "Joanna",
+    "no": "Liv"
+})[code]
 
 /**
  * Converts given text to audio data of 
@@ -16,13 +30,17 @@ export const textToAudio = async (text: string) => {
 
     const language = await getLanguage(text);
 
+    console.log(`Detected language: ${language}`);
+
     const command = new SynthesizeSpeechCommand({
         Text: text,
         OutputFormat: "mp3",
-        VoiceId: "Joanna" //TODO: choose voice depending on language
+        VoiceId: voiceFromLanguage(language)
     });
 
     const client = new PollyClient({ region: "eu-north-1" });
+
+
     const data = await client.send(command);
     return data.AudioStream
 }

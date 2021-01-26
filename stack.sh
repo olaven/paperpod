d44449a47f4bd516f7288ade6f9debc3c29ee14e
Olav Sundfør <olav@sundfoer.com>
2021-01-26 20:33:14 +0100

    got something running <3

--- stack.sh
+++ stack.sh
@@ -1,14 +1,1 @@
-env \
-    GATEWAY_PORT=8080 \
-    WEB_PORT=3000 \
-    API_PORT=8081 \
-    AUTHENTICATION_PORT=8082 \
-    JWT_SECRET="jwt-token-secret-stack" \
-    MONGODB_NAME=$MONGODB_NAME \ # these do not work rn, as no mongo is set
-    MONGODB_USERNAME=$MONGODB_USERNAME \ # these do not work rn, as no mongo is set
-    MONGODB_PASSWORD=$MONGODB_PASSWORD \ # these do not work rn, as no mongo is set
-    MONGODB_HOST=$MONGODB_HOST \ # these do not work rn, as no mongo is set
-    MONGODB_PORT=$MONGODB_PORT \ # 27017 \ # these do not work rn, as no mongo is set
-    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-    AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-    docker stack deploy -c stack.yml paperpod
+env GATEWAY_PORT=8080 WEB_PORT=3000 API_PORT=8081 AUTHENTICATION_PORT=8082 JWT_SECRET="jwt-token-secret-stack" MONGODB_NAME="SOMEMONONAME"MONGODB_USERNAME="SOMEMONGOUSERNAME" MONGODB_PASSWORD="SOMEMONGOPASSWORD" MONGODB_HOST="SOMEMOGNOHST" MONGODB_PORT=27017 AWS_ACCESS_KEY_ID="SOME ACCESS KEY" AWS_SECRET_ACCESS_KEY="SOME SECRET KEY" docker stack deploy -c stack.yml paperpod

0c46e46665a9571ea6d71c144354991ba7427a2a
Olav Sundfør <olav@sundfoer.com>
2021-02-02 21:27:59 +0100

    coverage is high enough on common

---
+++ packages/common/src/index.test.ts
@@ -0,0 +1,26 @@
+import * as index from "./index";
+/**
+ * NOTE:
+ * This file just checks that the
+ * modules that are expected to be exported,
+ * actually are exported.
+ *
+ * The idea is to increase general friction in the codebase
+ * , making it harder to accidentally remove something.
+ */
+
+describe("The exported modules from index", () => {
+
+    it("does export models", () => {
+        expect(index.models).toBeDefined();
+    })
+    it("does export validators", () => {
+        expect(index.validators).toBeDefined();
+    })
+    it("does export constants", () => {
+        expect(index.constants).toBeDefined();
+    })
+    it("does export test", () => {
+        expect(index.test).toBeDefined();
+    })
+});

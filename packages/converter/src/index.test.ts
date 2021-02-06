9d506624d89b3a744d8fd6eb5e23be0c8e8d52c9
Olav Sundfør <olav@sundfoer.com>
2021-02-06 18:44:55 +0100

    simplified file structure in converter

--- packages/converter/src/index.test.ts
+++ packages/converter/src/index.test.ts
@@ -1,25 +1,83 @@
-import * as index from "./index";
+import { test } from "@paperpod/common"
+import { withStorageUri } from ".";
+import { withTextualData, getRSSFeed } from "./index";
 
-describe("Exported data from index.ts", () => {
 
-    describe("Functions exported through converter", () => {
+describe("converter", () => {
 
-        it("Does export withStorageUri", () => {
+    jest.mock("puppeteer");
+    describe("withTextualData", () => {
 
-            expect(index.withStorageUri).toBeDefined();
+        it("Does not throw", () => {
+
+            expect(
+                withTextualData(
+                    test.mocks.article()
+                )
+            ).resolves.not.toThrow();
+        });
+
+        it("Does return replce article data", async () => {
+
+            const original = test.mocks.article();
+            const withData = await withTextualData(original);
+
+            expect(withData.text).not.toEqual(original.text);
+            expect(withData.title).not.toEqual(original.title);
+            expect(withData.author).not.toEqual(original.author);
+        });
+    });
+
+    describe("Getting RSS feed", () => {
+
+        it("does not crash", () => {
+
+            expect(() => {
+
+                getRSSFeed([
+                    test.mocks.article()
+                ])
+            }).not.toThrow();
+        });
+
+        //NOTE: this is properly unit tested in `rss.test.ts`
+        it("Does return something looking like an XML feed", () => {
+
+
+            const feed = getRSSFeed([
+                test.mocks.article()
+            ]);
+
+            expect(feed).toContain("<channel>");
+        });
+
+        it("Does return a feed with data from the articles", () => {
+
+            const articles = [
+                test.mocks.article(),
+                test.mocks.article(),
+                test.mocks.article(),
+            ];
+
+            const feed = getRSSFeed(articles);
+
+            articles.forEach(article => {
+                expect(feed).toContain(article.title);
+            });
+        });
+    });
+
+    describe("withStorageUri", () => {
+
+        jest.mock("@aws-sdk/client-polly");
+        jest.mock("@aws-sdk/client-comprehend");
+
+        it("Does not throw", async () => {
+
+            const article = await withStorageUri(
+                test.mocks.article()
+            )
+            expect(article).toBeDefined()
         })
-        it("Does export withTextualData", () => {
-
-            expect(index.withTextualData).toBeDefined();
-        })
-        it("Does export getRSSFeed", () => {
-
-            expect(index.getRSSFeed).toBeDefined();
-        })
-        it("Does export getAudioStream", () => {
-
-            expect(index.getAudioStream).toBeDefined();
-        })
-
-    })
-})
+    });
+});
--- packages/converter/src/index.ts
+++ packages/converter/src/index.ts
@@ -1,2 +1,46 @@
-export * from "./converter"
+import { models } from "@paperpod/common";
+import { getAudioStream as _getAudioStream } from "./storage";
+import { textToAudio } from "./audio";
+import { getTextualData } from "./text";
+import { convertToRSSFeed } from "./rss";
 
+/**
+ * Triggers conversions and points the result to AWS S3
+ * @param article 
+ */
+export const withStorageUri = async (article: models.Article): Promise<models.Article> => ({
+    ...article,
+    storage_uri: await textToAudio(article.text)
+});
+
+
+
+/**
+ * Converts given article url to textual data like, text, title, description and author
+ * @returns article with extracted text 
+ */
+export const withTextualData = async (article: {
+    _id: string,
+    owner_id: string,
+    original_url: string,
+    added_timestamp: number,
+    storage_uri: string,
+}): Promise<models.Article> => ({
+    ...article,
+    ...await getTextualData(article.original_url)
+});
+
+
+/**
+ * Convert list of articles to an RSS feed 
+ */
+export const getRSSFeed = (articles: models.Article[]) => {
+    return convertToRSSFeed(articles);
+}
+
+
+/**
+* Download the audio of a given article 
+* @param article 
+*/
+export const getAudioStream = _getAudioStream;

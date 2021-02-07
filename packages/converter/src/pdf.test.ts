94b9dfdd6016863214d0af8e4b32a7d1a2c120da
Olav Sundfør <olav@sundfoer.com>
2021-02-07 13:28:59 +0100

    externalized test pdfs and changed metadata access

--- packages/converter/src/pdf.test.ts
+++ packages/converter/src/pdf.test.ts
@@ -1,43 +1,54 @@
-import path from "path";
-import fs from "fs";
+import fs from "fs"
+import fetch from "node-fetch";
 import { pdfToArticle } from "./pdf";
 
 describe("Functions for converting pdf data to articles", () => {
 
-    const sampleBuffer = () =>
-        fs.readFileSync(
-            path.resolve("src", "pdf-sample.pdf")
+    const downloadPDF = async (url: string) => {
+
+        const response = await fetch(url);
+        const arrayBuffer = await response.arrayBuffer();
+
+        return Buffer.from(
+            arrayBuffer
         );
+    }
+
+    const convertComplexPDF = async () => pdfToArticle(
+        await downloadPDF("https://academicjournals.org/journal/IJCER/article-full-text-pdf/0208F0359126")
+    );
+
+    const convertSimplePDF = async () => pdfToArticle(
+        await downloadPDF("https://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf")
+    );
 
     it("is defined and can be called", () => {
 
 
         expect(async () => {
             //@ts-ignore 
-            await pdfToArticle(sampleBuffer())
+            await convertSimplePDF();
         }).not.toThrow();
     });
 
     it("Does accept a buffer as argument", () => {
 
         expect(async () => {
-            await pdfToArticle(sampleBuffer())
+            await convertSimplePDF();
         }).not.toThrow();
     });
 
 
     it("Does return some text from an actual pdf", async () => {
 
-        const buffer = sampleBuffer();
-        const article = await pdfToArticle(buffer);
-
+        const article = await convertSimplePDF();
         expect(article.text).toBeTruthy();//i.e. is not ""
     });
 
     it("Does return text containing some of the text from pdf", async () => {
 
         //NOTE: This tests assumes that the content of the test-PDF is https://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf
-        const article = await pdfToArticle(sampleBuffer());
+        const article = await convertSimplePDF();
         expect(article.text).toContain(
             "Adobe® Portable Document Format (PDF) is a universal file format that preserves all"
         );
@@ -45,13 +56,13 @@ describe("Functions for converting pdf data to articles", () => {
 
     it("Does return a title", async () => {
 
-        const article = await pdfToArticle(sampleBuffer());
+        const article = await convertSimplePDF();
         expect(article.title).toBeTruthy();
     });
 
     it("Does return a timestamp", async () => {
 
-        const article = await pdfToArticle(sampleBuffer());
+        const article = await convertSimplePDF();
 
         expect(typeof article.publication_timestamp).toEqual("number");
         expect(article.publication_timestamp).toBeTruthy();
@@ -60,16 +71,15 @@ describe("Functions for converting pdf data to articles", () => {
     it("Does return a timestamp with the correct year", async () => {
 
         //NOTE: This test assumes a specific sample-pdf; 
-        const article = await pdfToArticle(sampleBuffer());
+        const article = await convertSimplePDF();
         const year = new Date(article.publication_timestamp).getFullYear();
         expect(year).toEqual(2000);
     });
 
-
     it("Does return a timestamp with the correct month", async () => {
 
         //NOTE: This test assumes a specific sample-pdf; 
-        const article = await pdfToArticle(sampleBuffer());
+        const article = await convertSimplePDF();
         const month = new Date(article.publication_timestamp).getMonth();
         expect(month).toEqual(5);
     });
@@ -77,7 +87,19 @@ describe("Functions for converting pdf data to articles", () => {
     it("Does return an author field with PDF creator", async () => {
 
         //NOTE: This test assumes a specific sample-pdf; 
-        const article = await pdfToArticle(sampleBuffer());
+        const article = await convertSimplePDF();
         expect(article.author).toEqual("cdaily");
     });
+
+    it("Does not include footnotes in text", async () => {
+
+        const article = await convertComplexPDF();
+        expect(article).not.toContain(" it might be easier to position your work if you also tell the reader what your work will not do.");
+    });
+
+    it("Does extract author name from complex pdf", async () => {
+
+        const article = await convertComplexPDF();
+        expect(article.author).toBe("IEEE");
+    })
 });
--- packages/converter/src/pdf.ts
+++ packages/converter/src/pdf.ts
@@ -4,19 +4,44 @@ import pdf from "pdf-parse";
 export const pdfToArticle = async (stream: Buffer): Promise<Partial<models.Article>> => {
 
     const data = await pdf(stream);
-    const metadata = data.metadata._metadata;
+
+    const { Title, CreationDate, Author } = data.info;
     const { text } = data;
 
-    const title = metadata["dc:title"];
-    const author = metadata["dc:creator"];
-    const publication_timestamp = new Date(
-        metadata["xmp:createdate"]
-    ).getTime();
+
+    const publication_timestamp = parseDate(CreationDate);
 
     return {
         text,
-        title,
-        author,
         publication_timestamp,
+        title: Title,
+        author: Author,
     };
+}
+
+//NOTE: adapted from: https://github.com/mozilla/pdf.js/blob/1de1ae0be6849911430e087decb744936ac56366/src/scripting_api/doc.js#L150-L175
+export const parseDate = (date: string) => {
+
+    date = date.substring(2);
+    const year = date.substring(0, 4);
+    const month = date.substring(4, 6);
+    const day = date.substring(6, 8);
+    const hour = date.substring(8, 10);
+    const minute = date.substring(10, 12);
+    const o = date.charAt(12);
+
+    let second, offsetPos;
+
+    if (o === "Z" || o === "+" || o === "-") {
+        second = "00";
+        offsetPos = 12;
+    } else {
+        second = date.substring(12, 14);
+        offsetPos = 14;
+    }
+    //@ts-ignore
+    const offset = date.substring(offsetPos).replaceAll("'", "");
+    return new Date(
+        `${year}-${month}-${day}T${hour}:${minute}:${second}${offset}`
+    ).getTime();
 }

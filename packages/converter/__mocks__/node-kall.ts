964fb5ce7e077e86e1f6451d028fc2d1996514ff
Olav Sundfør <olav@sundfoer.com>
2021-02-09 22:21:38 +0100

    kall mock uses .get

--- packages/converter/__mocks__/node-kall.ts
+++ packages/converter/__mocks__/node-kall.ts
@@ -2,7 +2,7 @@ export const get = async () =>
     [200, {}];
 
 export const filterResponse = async () => ({
-    headers: () => ({
-        "Content-Type": "application/json" //NOTE: not pdf 
-    })
+    headers: {
+        get: () => "application/json" //NOTE: not pdf 
+    }
 })

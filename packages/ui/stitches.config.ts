e3216bef8a2acb6265574d2c23bc69c2c6bb7c3f
Olav Sundfør <olav@sundfoer.com>
2021-02-11 16:17:12 +0100

    added @paperpod/ui, with stitches config

---
+++ packages/ui/stitches.config.ts
@@ -0,0 +1,9 @@
+// stitches.config.ts
+import { createStyled } from '@stitches/react';
+
+export const { styled, css } = createStyled({
+    prefix: '',
+    tokens: {},
+    breakpoints: {},
+    utils: {},
+});

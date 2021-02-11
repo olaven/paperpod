6c9229f0a0e3656298a1145e815e734d1dfc7b7a
Olav Sundfør <olav@sundfoer.com>
2021-02-11 16:23:02 +0100

    created basic styled button

---
+++ packages/ui/src/button.ts
@@ -0,0 +1,9 @@
+import { styled } from "./stitches.config";
+
+styled("button", {
+    backgroundColor: "grey",
+    ":hover": {
+        backgroundColor: "white",
+        transform: "scale(1.1)"
+    }
+}); 

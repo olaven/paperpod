06394abf6df9ff3334ea8ec278df1078d3571d74
Olav Sundfør <olav@sundfoer.com>
2021-02-15 13:36:22 +0100

    chrome extension works with react/typesrcipt/@paperpod/ui

--- packages/extension/src/index.tsx
+++ packages/extension/src/index.tsx
@@ -1,10 +1,13 @@
-
+import { Input } from "@paperpod/ui";
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 console.log('Hello from tsx!')
 
 ReactDOM.render(
-    <p>Hello</p>,
+    <>
+        Her er det tekst
+        <Input placeholder="test" />
+    </>,
     document.getElementById('root'),
 )

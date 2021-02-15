40777f4c19463f312e8ee2f8ac6f04a97fb4608b
Olav Sundfør <olav@sundfoer.com>
2021-02-15 16:13:36 +0100

    added notice about unfinished version in footer

--- packages/web/components/layout/footer.tsx
+++ packages/web/components/layout/footer.tsx
@@ -1,3 +1,4 @@
 export const Footer = () => <div>
     <p>Paperpod is made by Krets AS</p>
+    <p>You are using an unfinished version of Paperpod. Visit <a href="https://github.com/olaven/paperpod">github</a> to follow development</p>
 </div>

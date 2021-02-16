716f254923ebd15df6813be87125c7bea3c93fbd
Olav Sundfør <olav@sundfoer.com>
2021-02-16 10:34:44 +0100

    migrated to stitches beta

--- packages/ui/src/stitches.config.ts
+++ packages/ui/src/stitches.config.ts
@@ -1,42 +1,42 @@
 // stitches.config.ts
-import { createStyled } from '@stitches/react';
+import { createCss } from '@stitches/react';
 
-export const { styled, css } = createStyled({
-    tokens: {
+export const { styled, css, getCssString } = createCss({
+    theme: {
         colors: {
-            $primary: 'white',
-            $secondary: 'black',
-            $grey: 'hsl(0, 0%, 36%)',
+            primary: 'white',
+            secondary: 'black',
+            grey: 'hsl(0, 0%, 36%)',
         },
         space: {
-            $1: '1px',
-            $2: '2px',
-            $3: '3px',
-            $5: '5px',
-            $8: '8px',
-            $13: '13px',
-            $21: '21px',
-            $34: '34px',
-            $55: '55px',
-            $79: '79px',
-            $114: '114px',
+            1: '1px',
+            2: '2px',
+            3: '3px',
+            5: '5px',
+            8: '8px',
+            13: '13px',
+            21: '21px',
+            34: '34px',
+            55: '55px',
+            79: '79px',
+            114: '114px',
         },
         fontSizes: {
-            $5: '5px',
-            $8: '8px',
-            $13: '13px',
-            $21: '21px',
-            $34: '34px',
-            $55: '55px',
-            $79: '79px',
-            $114: '114px',
-            $223: '223px',
-            $367: '367px',
+            5: '5px',
+            8: '8px',
+            13: '13px',
+            21: '21px',
+            34: '34px',
+            55: '55px',
+            79: '79px',
+            114: '114px',
+            223: '223px',
+            367: '367px',
         }
     },
-    breakpoints: {
-        small: (rule) => `@media (max-width: 768px) { ${rule} }`,
-        large: rule => `@media (min-width: 768px) { ${rule} }`
+    conditions: {
+        small: `@media (max-width: 768px)`,
+        large: `@media (min-width: 768px)`,
     },
     utils: {},
 })

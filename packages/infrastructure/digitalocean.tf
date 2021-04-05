c36a28aad85089d5d1997c6de77304b199a3234b
Olav Sundfør <olav@sundfoer.com>
2021-04-05 13:01:23 +0200

    plan with a basic droplet works

--- packages/infrastructure/digitalocean.tf
+++ packages/infrastructure/digitalocean.tf
@@ -3,19 +3,22 @@ variable "do_token" {
   description = "token for Digitalocean"
   sensitive   = true
 }
-variable "pvt_key" {
+/* variable "pvt_key" {
   type        = string
   description = "Private key for Digitalocean, getting SSH to work"
   sensitive   = true
-}
+} */
 
 # Making SSH available for the rest of the script 
-data "digitalocean_ssh_key" "terraform" {
+/* data "digitalocean_ssh_key" "terraform" {
   name = "terraform"
-}
+} */
 
 # See https://www.digitalocean.com/community/tutorials/how-to-use-terraform-with-digitalocean for more details on ssh in the future
 
-resource "digitalocean_droplet" "www-1" {
-
+resource "digitalocean_droplet" "paperpod-manager-droplet" {
+  name   = "paperpod-manager"
+  image  = "docker-20-04"
+  size   = "s-1vcpu-1gb"
+  region = "ams1"
 }

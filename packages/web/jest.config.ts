dd819173ba950fadf4983f282bbf7623f26a1387
Olav Sundfør <olav@sundfoer.com>
2021-05-02 12:28:43 +0200

    WIP: playing around with jest config

--- packages/web/jest.config.ts
+++ packages/web/jest.config.ts
@@ -2,5 +2,11 @@ import config from "../../jest.config";
 
 export default {
   ...config,
+  /* transform: {
+    "/\.[jt]sx?$/": "babel-jest"
+  },
+  transformIgnorePatterns: [
+    "node_modules", 
+  ], */
   setupFilesAfterEnv: ["./jest.setup.ts"]
 };

6f841bbcc5028cf617f81fd4b9cee9b400e99086
Olav Sundfør <olav@sundfoer.com>
2021-05-08 22:02:48 +0200

    logging db env on auth startup

--- packages/authentication/src/index.ts
+++ packages/authentication/src/index.ts
@@ -1,4 +1,14 @@
 import { boot } from "@paperpod/server";
 import { app } from "./app";
 
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGSSLMODE } = process.env;
+console.log(`
+    Going to start authentication with database envs: 
+    PGHOST: ${PGHOST},
+    PGDATABASE: ${PGDATABASE},
+    PGUSER: ${PGUSER},
+    PGPASSWORD: ${PGPASSWORD},
+    PGSSLMODE: ${PGSSLMODE},
+`);
+
 boot("/authentication", app);

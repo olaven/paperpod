b49a71dccdd1eefb441eb8626edbab210d30204b
Olav Sundfør <olav@sundfoer.com>
2021-05-24 19:32:56 +0200

    wrote react-hook like API for chrome storage

--- packages/extension/src/index.tsx
+++ packages/extension/src/index.tsx
@@ -3,27 +3,4 @@ import ReactDOM from "react-dom";
 import { AuthenticationPopup } from "./components/authentication/authenticationpopup";
 import { get, store } from "./storage/storage";
 
-const StorageTest = () => {
-  return (
-    <div>
-      <button
-        onClick={async () => {
-          await store("paperpod.logged_in", true);
-        }}
-      >
-        store
-      </button>
-
-      <button
-        onClick={async () => {
-          const result = await get<boolean>("paperpod.logged_in");
-          console.log(`result was ${result}`);
-        }}
-      >
-        get
-      </button>
-    </div>
-  );
-};
-
-ReactDOM.render(<StorageTest />, document.getElementById("root"));
+ReactDOM.render(<AuthenticationPopup />, document.getElementById("root"));
--- packages/extension/src/storage/storage.ts
+++ packages/extension/src/storage/storage.ts
@@ -1,15 +1,29 @@
 type Key = "paperpod.session_token" | "paperpod.logged_in"; // add more when needed
 
-export const store = <T>(key: Key, value: T) =>
-  new Promise<void>((resolve, reject) => {
-    chrome.storage.sync.set({ [key]: value }, () => {
-      resolve();
-    });
-  });
+export const useLoggedInStorage = () =>
+  usePermanentStorage<boolean>("paperpod.logged_in");
 
-export const get = <T> (key: Key) =>
-  new Promise<T>((resolve, reject) => {
-    chrome.storage.sync.get([key], (result) => {
-      resolve(result[key]);
+export const useSessionStorage = () =>
+  usePermanentStorage<string>("paperpod.logged_in");
+
+const save =
+  <T>(key: Key) =>
+  (value: T) =>
+    new Promise<void>((resolve, reject) => {
+      chrome.storage.sync.set({ [key]: value }, () => {
+        resolve();
+      });
     });
-  });
+
+const retrieve =
+  <T>(key: Key) =>
+  () =>
+    new Promise<T>((resolve, reject) => {
+      chrome.storage.sync.get([key], (result) => {
+        resolve(result[key]);
+      });
+    });
+
+const usePermanentStorage = <T>(key: Key) => {
+  return [save<T>(key), retrieve<T>(key)];
+};

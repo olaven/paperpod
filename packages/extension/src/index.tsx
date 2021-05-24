57ecf311744b3beb1eed2180f85e1dc01f98d7b9
Olav Sundfør <olav@sundfoer.com>
2021-05-24 19:35:13 +0200

    removed broken import

--- packages/extension/src/index.tsx
+++ packages/extension/src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import { AuthenticationPopup } from "./components/authentication/authenticationpopup";
-import { get, store } from "./storage/storage";
 
 ReactDOM.render(<AuthenticationPopup />, document.getElementById("root"));

9930f2b28cfb12197143405813d61df9cf79e82d
Olav Sundfør <olav@sundfoer.com>
2021-05-25 19:42:36 +0200

    removed http -> https redirect

--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -1,6 +1,4 @@
-import fs from "fs";
 import http from "http";
-import https from "https";
 import * as server from "@paperpod/server";
 import { withProxies, mapping } from "./proxy";
 import { logger } from "@paperpod/common";
@@ -19,40 +17,6 @@ export const app = withProxies(
   server.app.appWithEnvironment()
 );
 
-const actualServer =
-  // process.env.NODE_ENV === "production"
-  false //FIXME: add back production check when certs are fixed
-    ? https.createServer(
-        {
-          key: fs.readFileSync(
-            "/etc/letsencrypt/live/paperpod.fm/privkey.pem",
-            "utf8"
-          ),
-          cert: fs.readFileSync(
-            "/etc/letsencrypt/live/paperpod.fm/cert.pem",
-            "utf8"
-          ),
-          ca: fs.readFileSync(
-            "/etc/letsencrypt/live/paperpod.fm/chain.pem",
-            "utf8"
-          ),
-        },
-        app
-      )
-    : http.createServer(app);
-
-const redirectServer = http.createServer(
-  server.app.appWithEnvironment().use((request, response) => {
-    response.redirect(`https://${request.headers.host}${request.url}`);
-  })
-);
-
-redirectServer.listen(process.env.GATEWAY_HTTP_PORT, () => {
-  logger.info(
-    `Redirecting to HTTPS from port ${process.env.GATEWAY_HTTP_PORT}`
-  );
-});
-
-actualServer.listen(process.env.PORT, () => {
+http.createServer(app).listen(process.env.PORT, () => {
   logger.info(`Listening on ${process.env.PORT}`);
 });

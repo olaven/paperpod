2611bc3f2633d6209841c9319b254eb7f2ae864c
Olav Sundfør <olav@sundfoer.com>
2021-06-13 13:13:52 +0200

    articles and feed in home

--- packages/web/pages/home.tsx
+++ packages/web/pages/home.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { authentication, fetchers, FrontendContext } from "@paperpod/frontend";
 import { Button } from "@paperpod/ui";
+import { Articles } from "../components/articles/Articles";
+import { Feed } from "../components/feed/Feed";
 
 const Home = () => {
   const { user, token, setToken } = React.useContext(
@@ -16,6 +18,8 @@ const Home = () => {
     <div>
       you are logged in as {user.email}
       <Button onClick={onLogOut}>Log out</Button>
+      <Articles />
+      <Feed />
     </div>
   ) : (
     <div>loading.</div>

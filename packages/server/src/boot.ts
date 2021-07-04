d7e3cf2d5d7527f9fe5fea4f5cf7c8ebf484d527
Olav Sundfør <olav@sundfoer.com>
2021-07-04 23:38:11 +0200

    Added healthpoints
    Each service has a healthpoint available
    at `GET <service>/health`. (web has at /web-health.json`).
    
    Additionally, `GET /health/all` returns the
    healh status for all services.

--- packages/server/src/boot.ts
+++ packages/server/src/boot.ts
@@ -1,10 +1,41 @@
 import { logger } from "@paperpod/common";
+import readPkg from "read-pkg-up";
 import express from "express";
 import { appWithEnvironment } from "./app/appWithEnvironment";
 
-export const boot = (path: string, app = appWithEnvironment()) =>
+const port = process.env.PORT;
+
+/**
+ * Returns a nice health response
+ * for the service
+ * @param id the identifier of the service
+ * @returns
+ */
+const createHealthHandler =
+  (id: string) =>
+  async (request: express.Request, response: express.Response) => {
+    const {
+      packageJson: { version, name },
+    } = await readPkg();
+    return response.send({
+      name,
+      version,
+      message: `'${id}' is live ヽ(^。^)ノ`,
+    });
+  };
+
+export const boot = (
+  path: string,
+  app = appWithEnvironment(),
+  id = path.replace("/", "")
+) =>
   express()
+    .get(`${path}/health`, createHealthHandler(id))
     .use(path, app)
-    .listen(process.env.PORT, () => {
-      logger.info(path, "is listening on", process.env.PORT);
+    .listen(port, () => {
+      logger.info({
+        path,
+        port,
+        status: `listening`,
+      });
     });

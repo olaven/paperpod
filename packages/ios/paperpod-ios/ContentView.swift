d744d569e4bf034bd3a10b4d810223debf1c71df
Olav Sundfør <olav@sundfoer.com>
2021-07-10 20:11:14 +0200

    initialized xcode project

---
+++ packages/ios/paperpod-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  paperpod-ios
+//
+//  Created by Olav Sundfør on 10/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ packages/ios/paperpod-ios/paperpod_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  paperpod_iosApp.swift
+//  paperpod-ios
+//
+//  Created by Olav Sundfør on 10/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct paperpod_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

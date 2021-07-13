6e9a4575f35501d36efeb1fd180ac1cc8dc85c3a
Olav Sundfør <olav@sundfoer.com>
2021-07-13 23:10:36 +0200

    react native is running

--- packages/common/src/constants/constants.ts
+++ packages/common/src/constants/constants.ts
@@ -6,7 +6,7 @@ logger.debug({
 });
 
 export const APPLICATION_URL = () =>
-  process.env.NODE_ENV !== "production"
+  process.env.NODE_ENV === "dev" || process.env.NODE_ENV === "test"
     ? "http://localhost:8080"
     : `https://paperpod.fm`; //TODO: more elegant dev-variation
 export const TOKEN_COOKIE_HEADER = () => "x-paperpod-token-cookie";
--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -5,51 +5,25 @@
  * Thanks <3
  */
 
-const blacklist = require("metro-config/src/defaults/blacklist");
 const getWorkspaces = require("get-yarn-workspaces");
 const path = require("path");
 
 function getConfig(appDir, options = {}) {
-  console.log("The app dir is", appDir);
-
+  console.log("THE APP DIR IS");
   const workspaces = getWorkspaces(appDir);
 
   // Add additional Yarn workspace package roots to the module map
   // https://bit.ly/2LHHTP0
+
   const watchFolders = [
-    path.resolve(appDir, "..", "node_modules"),
+    path.resolve(appDir, "node_modules"),
     ...workspaces.filter((workspaceDir) => !(workspaceDir === appDir)),
   ];
 
+  console.log({ watchFolders });
+
   return {
     watchFolders,
-    resolver: {
-      blacklistRE: blacklist([
-        // Ignore other resolved react-native installations outside of
-        // mobile-native - this prevents a module naming collision when mapped.
-        /^((?!mobile-native).)+[\/\\]node_modules[/\\]react-native[/\\].*/,
-
-        // Ignore react-native-svg dependency in mobile-ui, mapped below.
-        // react-native-svg must only be included once due to a side-effect. It
-        // has not been hoisted as it requires native module linking here.
-        // http://bit.ly/2LJ7V4b
-        /mobile-ui[\/\\]node_modules[/\\]react-native-svg[/\\].*/,
-      ]),
-      extraNodeModules: {
-        // Resolve all react-native module imports to the locally-installed version
-        "react-native": path.resolve(appDir, "node_modules", "react-native"),
-
-        // Resolve additional nohoist modules depended on by other packages
-        "react-native-svg": path.resolve(
-          appDir,
-          "node_modules",
-          "react-native-svg"
-        ),
-
-        // Resolve core-js imports to the locally installed version
-        "core-js": path.resolve(appDir, "node_modules", "core-js"),
-      },
-    },
   };
 }
 

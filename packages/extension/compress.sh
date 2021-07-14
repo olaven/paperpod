f452aadd5e161ece3dbe12fce703e049fab6fffe
Olav Sundfør <olav@sundfoer.com>
2021-07-14 22:35:06 +0200

    comparing manifest and package.json in compress

--- packages/extension/compress.sh
+++ packages/extension/compress.sh
@@ -1,11 +1,24 @@
 CURRENT_DIR=$(pwd)
-PACKAGE_VERSION=$(cat package.json \
+PACKAGE_VERSION_MANIFEST=$(cat manifest.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 
+PACKAGE_VERSION_NODE=$(cat manifest.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+
+if [[ $PACKAGE_VERSION_NODE != $PACKAGE_VERSION_MANIFEST ]]
+then 
+  echo "Versions in manifest.json and package.json arent' matching"; 
+  exit 1; 
+fi
 
 if [[ $CURRENT_DIR != *extension ]]
 then
@@ -19,4 +32,4 @@ fi
 
 rm -rf node_modules;
 yarn extension install --production=true;
-zip -r extension-${PACKAGE_VERSION}.zip . -x */\.* *.git* \.* *.zip *.swp *src* *.cache* jest.setup.ts jest.config.ts
+zip -r extension-${PACKAGE_VERSION_MANIFEST}.zip . -x */\.* *.git* \.* *.zip *.swp *src* *.cache* jest.setup.ts jest.config.ts

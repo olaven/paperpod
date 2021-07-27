23dbe2574dbc4e37b59cf2b9bd5d85ca51222347
Olav Sundfør <olav@sundfoer.com>
2021-07-27 22:09:47 +0200

    new guide

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,30 +1,13 @@
-/**
- * Heavily inspired by
- * [@hunties article](https://medium.com/@huntie/a-concise-guide-to-configuring-react-native-with-yarn-workspaces-d7efa71b6906).
- *
- * Thanks <3
- */
-
-const getWorkspaces = require("get-yarn-workspaces");
 const path = require("path");
 
-function getConfig(appDir, options = {}) {
-  console.log("THE APP DIR IS");
-  const workspaces = getWorkspaces(appDir);
-
-  // Add additional Yarn workspace package roots to the module map
-  // https://bit.ly/2LHHTP0
-
-  const watchFolders = [
-    path.resolve(appDir, "node_modules"),
-    ...workspaces.filter((workspaceDir) => !(workspaceDir === appDir)),
-  ];
-
-  console.log({ watchFolders });
-
-  return {
-    watchFolders,
-  };
-}
-
-module.exports = getConfig(__dirname);
+module.exports = {
+  watchFolders: [path.resolve(__dirname, "../../")],
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};

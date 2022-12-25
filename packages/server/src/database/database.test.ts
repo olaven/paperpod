f2d63537c02e07deaa887e80d1593b578bc52028
Olav Sundfør <olav@sundfoer.com>
2022-12-25 23:36:54 +0100

    process.env.PAPERPOD_SCHEMA validation checks for valid values (#203)
    * process.env.PAPERPOD_SCHEMA validation checks for valid values
    
    * Fixed test

--- packages/server/src/database/database.test.ts
+++ packages/server/src/database/database.test.ts
@@ -1,17 +1,49 @@
+import faker from "faker";
 import { database } from "./database";
+import { validSchemaNames } from "./migrate";
+
+export const withMockedSchemaVariable = async (schema: string | undefined, action: () => void | Promise<void>) => async () => {
+  const original = global.process.env.PAPERPOD_SCHEMA; 
+  global.process.env.PAPERPOD_SCHEMA = schema; 
+  await action();
+  global.process.env.PAPERPOD_SCHEMA = original
+};
 
 describe("The database module", () => {
   jest.mock("klart");
   //jest.spyOn(klart, "withConfiguration").mockImplementation(jest.fn());
 
-  it("Does not throw", () => {
-    expect(database()).resolves.not.toThrow();
+  it(`Does not throw for values in ${validSchemaNames}`, () => {
+    for (const schema of validSchemaNames ) {
+      withMockedSchemaVariable(schema, () => {
+        expect(() => database()).not.toThrow()
+      });
+    }
   });
 
+  it("Throws if the schema name is not a valid one" , () => {
+
+    const schema = faker.name.firstName()
+    withMockedSchemaVariable(schema, () => {
+      expect(() => database()).rejects.toThrow(`Schema ${schema} not one of`)
+    }); 
+  }); 
+
+  it("Throws if the schema name is not defined" , () => {
+
+    const schema = undefined; 
+    withMockedSchemaVariable(schema, () => {
+      expect(() => database()).rejects.toThrow(`Schema needs to be defined in env: undefined`)
+    });
+  }); 
+
   it("Returns a database instance", async () => {
-    const db = await database();
-    expect(db.first).toBeDefined();
-    expect(db.rows).toBeDefined();
-    expect(db.run).toBeDefined();
+
+    await withMockedSchemaVariable("authentication", async () => {
+      const db = await database();
+      expect(db.first).toBeDefined();
+      expect(db.rows).toBeDefined();
+      expect(db.run).toBeDefined();
+    }); 
   });
 });
--- packages/server/src/database/database.ts
+++ packages/server/src/database/database.ts
@@ -1,11 +1,14 @@
-import { withConfiguration } from "klart";
 import { logger } from "@paperpod/common";
+import { withConfiguration } from "klart";
 import { getConfiguration } from "./configuration";
-import { SchemaName } from "./migrate";
+import { SchemaName, validSchemaNames } from "./migrate";
 
 export const database = async () => {
   const schema = process.env.PAPERPOD_SCHEMA as SchemaName;
   if (!schema) throw `Schema needs to be defined in env: ${schema}`;
+  if (!validSchemaNames.includes(schema)) {
+    throw `Schema ${schema} not one of ${validSchemaNames}`
+  }
 
   const configuration = getConfiguration();
   logger.trace({

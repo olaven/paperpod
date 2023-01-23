f7418cc0030db49c0fb604c421e49d25d514661d
olaven <olav@sundfoer.com>
2023-01-23 23:32:32 +0100

    Lowered treshold

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -2,12 +2,4 @@ const config = require("../../jest.config");
 
 module.exports = {
   ...config,
-  coverageThreshold: {
-    global: {
-      branches: 75,
-      functions: 75,
-      lines: 75,
-      statements: 75,
-    },
-  },
 };

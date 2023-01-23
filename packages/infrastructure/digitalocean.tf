f087504e502a17f8fea49542b3878145da658e41
Olav Sundfør <olav@sundfoer.com>
2023-01-23 15:58:11 +0100

    Simplified App Deployment (massively) (#208)

--- packages/infrastructure/digitalocean.tf
+++ packages/infrastructure/digitalocean.tf
@@ -4,6 +4,85 @@ variable "do_token" {
   sensitive   = true
 }
 
+variable "gateway_port" {
+  type = number 
+  description = "the port for gateway"
+  sensitive = true
+}
+variable "web_port" {
+  type = number 
+  description = "the port for web"
+  sensitive = true
+}
+variable "docs_port" {
+  type = number 
+  description = "the port for docs"
+  sensitive = true
+}
+variable "api_port" {
+  type = number 
+  description = "the port for api"
+  sensitive = true
+}
+variable "authentication_port" {
+  type = number 
+  description = "the port for authentication"
+  sensitive = true
+}
+variable "log_level" {
+  type = string 
+  description = "the port for log"
+  sensitive = true
+}
+
+variable "stripe_api_key" {
+  type = string 
+  description = "the private stripe API key"
+}
+
+variable "stripe_publishable_key" {
+  type = string 
+  description = "the PUBLIC stripe API key"
+}
+
+## API 
+variable "authentication_internal_port" {
+  type = number 
+  description = "the port for authentication_internal"
+  sensitive = true
+}
+
+variable "admin_username" {
+  type = string
+  description = "admin username"
+  sensitive = true
+}
+
+variable "admin_password" {
+  type = string
+  description = "admin password"
+  sensitive = true
+}
+
+variable "jwt_secret" {
+  type = string
+  description = "secret for encrypting JWT's"
+  sensitive = true
+}
+
+variable "aws_access_key_id" {
+  type = string
+  description = "AWS Access Key ID"
+  sensitive = true
+}
+
+variable "aws_secret_access_key" {
+  type = string
+  description = "AWS Secret Access Key"
+  sensitive = true
+}
+
+
 provider "digitalocean" {
   token = var.do_token
 }
@@ -18,92 +97,14 @@ resource "digitalocean_project" "paperpod-project" {
 resource "digitalocean_project_resources" "project-to-resource-mapping" {
   project = digitalocean_project.paperpod-project.id
   resources = [
-    digitalocean_droplet.manager-droplet.urn,
-    digitalocean_database_cluster.database-cluster.urn,
-    digitalocean_domain.default.urn,
-    digitalocean_loadbalancer.public.urn
+    digitalocean_app.paperpod-app.urn, digitalocean_domain.default.urn,  digitalocean_database_cluster.database-cluster.urn,
   ]
-}
-
-resource "digitalocean_certificate" "cert" {
-  // may needt to change `.name` on update..
-  // https://github.com/digitalocean/terraform-provider-digitalocean/issues/98#issuecomment-417315778
-  name    = "paperpod-certificate-app"
-  type    = "lets_encrypt"
-  domains = [digitalocean_domain.default.name]
-
-  lifecycle {
-    create_before_destroy = true 
-  }
-}
-
-# Create a new Load Balancer with TLS termination
-resource "digitalocean_loadbalancer" "public" {
-  name        = "paperpod-loadbalancer"
-  region      = "ams3"
-  vpc_uuid = digitalocean_vpc.network.id
-  droplet_ids = [
-    digitalocean_droplet.manager-droplet.id
-  ]
-
-  forwarding_rule {
-    entry_port     = 443
-    entry_protocol = "https"
-
-    target_port     = 80
-    target_protocol = "http"
-
-    certificate_name = digitalocean_certificate.cert.name
-  }
-
-  healthcheck { 
-    protocol = "http"
-    port = 80
-    path = "/"
-  }
-}
-
-resource "digitalocean_database_firewall" "droplet-to-database-firewall" {
-  cluster_id = digitalocean_database_cluster.database-cluster.id
-
-  rule {
-    type  = "droplet"
-    value = digitalocean_droplet.manager-droplet.id
-  }
 }
 
 resource "digitalocean_domain" "default" {
   name       = "paperpod.fm"
-  ip_address = digitalocean_droplet.manager-droplet.ipv4_address
 }
 
-resource "digitalocean_record" "www" {
-  domain = digitalocean_domain.default.name
-  type = "A"
-  name = "www"
-  value = digitalocean_loadbalancer.public.ip
-}
-resource "digitalocean_record" "landing_page_cname" {
-  domain = digitalocean_domain.default.name
-  type = "CNAME"
-  name = "landingpage"
-  value = "olaven.org."
-}
-resource "digitalocean_vpc" "network" {
-  name = "paperpod-network"
-  description = "Private network for Paperpod services"
-  region = "ams3"
-}
-
-resource "digitalocean_droplet" "manager-droplet" {
-  
-  name   = "paperpod-manager"
-  image  = "docker-20-04"
-  size   = "s-1vcpu-2gb"
-  region = "ams3"
-  private_networking = true
-  vpc_uuid = digitalocean_vpc.network.id
-}
 
 resource "digitalocean_database_cluster" "database-cluster" {
   name       = "database-cluster"
@@ -112,66 +113,343 @@ resource "digitalocean_database_cluster" "database-cluster" {
   size       = "db-s-1vcpu-1gb"
   region     = "ams3"
   node_count = 1
-  private_network_uuid = digitalocean_vpc.network.id
 }
 
-resource "digitalocean_database_firewall" "db_firewall" {
-  cluster_id = digitalocean_database_cluster.database-cluster.id 
 
-  rule {
-    type = "droplet"
-    value = digitalocean_droplet.manager-droplet.id
-  }
-}
-
-/*
-Useless because of: 
-> Any new users created will always have normal role, 
-> only the default user that comes with database cluster 
-> creation has primary role. Additional permissions must be managed manually.
-- https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/resources/database_user
-*/
-#resource "digitalocean_database_user" "user" {
-#  cluster_id = digitalocean_database_cluster.database-cluster.id
-#  name = "paperpod_user"
-#}
-
-resource "digitalocean_database_db" "database" {
-  name       = "paperpod"
+data "digitalocean_database_ca" "ca" {
   cluster_id = digitalocean_database_cluster.database-cluster.id
 }
 
-output "droplet_id" {
-  sensitive  = true
-  value = digitalocean_droplet.manager-droplet.id
-}
-output "droplet_uri" {
-  sensitive  = true
-  value = digitalocean_droplet.manager-droplet.ipv4_address
-}
-output "droplet_uri_private" {
-  sensitive  = true
-  value = digitalocean_droplet.manager-droplet.ipv4_address_private
-}
 
-output "database_host"  {
-  sensitive = true
-  value = digitalocean_database_cluster.database-cluster.private_host
-}
-output "database_database" {
-  sensitive = true
-  value = digitalocean_database_db.database.name
-}
-output "database_user" {
-  sensitive = true
-  value = digitalocean_database_cluster.database-cluster.user 
-}
-output "database_password" {
-  sensitive = true
-  value = digitalocean_database_cluster.database-cluster.password
-}
+resource "digitalocean_app" "paperpod-app" {
+  spec {
+    name    = "paperpod"
+    region  = "ams"
+    domain {
+      name = "paperpod.fm"
+      zone = digitalocean_domain.default.name
+    }
 
-output "database_port" {
-  sensitive = true 
-  value = digitalocean_database_cluster.database-cluster.port
+    database {
+      name       = "db"
+      engine     = "PG"
+      production = true
+      cluster_name = digitalocean_database_cluster.database-cluster.name
+    }
+    
+    alert {
+      rule = "DEPLOYMENT_FAILED"
+    }
+    
+    service {
+      name               = "gateway"
+      instance_count     = 1
+      instance_size_slug = "basic-xxs"
+
+      github {
+        branch         = "main"
+        deploy_on_push = true
+        repo           = "olaven/paperpod"
+      }
+
+      dockerfile_path = "dockerfiles/Dockerfile.gateway"
+      http_port = var.gateway_port
+
+      routes { 
+        path = "/"
+      }
+
+      env { 
+        key = "PORT"
+        value = var.gateway_port
+      }
+
+      env { 
+        key = "WEB_PORT"
+        value = var.web_port
+      }
+
+      env { 
+        key = "DOCS_PORT"
+        value = var.docs_port
+      }
+      
+      env { 
+        key = "API_PORT"
+        value = var.api_port
+      }
+      
+      env { 
+        key = "AUTHENTICATION_PORT"
+        value = var.authentication_port
+      }
+      
+      env { 
+        key = "NODE_ENV"
+        value = "production"
+      }
+      
+      env { 
+        key = "LOG_LEVEL"
+        value = var.log_level
+      }
+
+      alert {
+        value    = 75
+        operator = "GREATER_THAN"
+        window   = "TEN_MINUTES"
+        rule     = "CPU_UTILIZATION"
+      }
+    }
+    service {
+      name               = "web"
+      instance_count     = 1
+      instance_size_slug = "basic-xxs"
+
+      github {
+        branch         = "main"
+        deploy_on_push = true
+        repo           = "olaven/paperpod"
+      }
+
+      dockerfile_path = "dockerfiles/Dockerfile.web"
+      internal_ports = [var.web_port]
+
+      env {
+        key = "LOG_LEVEL"
+        value = var.log_level
+      }
+
+      env {
+        key = "NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY"
+        value = var.stripe_publishable_key
+      }
+    }
+
+    service {
+      name               = "api"
+      instance_count     = 1
+      instance_size_slug = "basic-xxs"
+
+      github {
+        branch         = "main"
+        deploy_on_push = true
+        repo           = "olaven/paperpod"
+      }
+
+      dockerfile_path = "dockerfiles/Dockerfile.api"
+      internal_ports = [var.api_port]
+
+       env { 
+        key = "PORT"
+        value = var.api_port
+      }
+
+      env {
+        key = "AUTHENTICATION_INTERNAL_PORT"
+        value = var.authentication_internal_port
+      }
+
+      env {
+        key = "ADMIN_USERNAME"
+        value = var.admin_username
+      }
+
+      env {
+        key = "ADMIN_PASSWORD"
+        value = var.admin_password
+      }
+
+      env {
+        key = "JWT_SECRET"
+        value = var.jwt_secret
+      }
+      
+      env {
+        key = "AUTHENTICATION_PORT"
+        value = var.authentication_port
+      }
+      
+      env {
+        key = "PGPORT"
+        value = "$${db.PORT}"
+      }
+      env {
+        key = "PGHOST"
+        value = "$${db.HOSTNAME}"
+      }
+
+      env {
+        key = "PGDATABASE"
+        value = "$${db.DATABASE}"
+      }
+
+      env {
+        key = "PGUSER"
+        value = "$${db.USERNAME}"
+      }
+
+      env {
+        key = "PGPASSWORD"
+        value = "$${db.PASSWORD}"
+      }
+
+      env {
+        key = "DATABASE_CA"
+        value = "$${db.CA_CERT}"
+      }
+
+      env {
+        key = "PAPERPOD_SCHEMA"
+        value = "api"
+      }
+
+
+      env {
+        key = "AWS_ACCESS_KEY_ID"
+        value = var.aws_access_key_id
+      }
+      
+      env {
+        key = "AWS_SECRET_ACCESS_KEY"
+        value = var.aws_secret_access_key
+      }
+
+      env {
+        key = "LOG_LEVEL"
+        value = var.log_level
+      }
+      
+      env {
+        key = "PGPORT"
+        value = digitalocean_database_cluster.database-cluster.port
+      }
+
+      alert {
+        value    = 75
+        operator = "GREATER_THAN"
+        window   = "TEN_MINUTES"
+        rule     = "CPU_UTILIZATION"
+      }
+    }
+
+    service {
+      name               = "docs"
+      instance_count     = 1
+      instance_size_slug = "basic-xxs"
+
+      github {
+        branch         = "main"
+        deploy_on_push = true
+        repo           = "olaven/paperpod"
+      }
+
+
+      dockerfile_path = "dockerfiles/Dockerfile.docs"
+      internal_ports = [var.docs_port]
+
+      alert {
+        value    = 75
+        operator = "GREATER_THAN"
+        window   = "TEN_MINUTES"
+        rule     = "CPU_UTILIZATION"
+      }
+    }
+
+     service {
+      name               = "authentication"
+      instance_count     = 1
+      instance_size_slug = "basic-xxs"
+
+      internal_ports = [var.authentication_port, var.authentication_internal_port]
+
+      github {
+        branch         = "main"
+        deploy_on_push = true
+        repo           = "olaven/paperpod"
+      }
+
+      dockerfile_path = "dockerfiles/Dockerfile.authentication"
+
+    
+
+      env { 
+        key = "PORT"
+        value = var.authentication_port
+      }
+
+      env {
+        key = "INTERNAL_PORT"
+        value = var.authentication_internal_port
+      }
+
+
+      env {
+        key = "ADMIN_USERNAME"
+        value = var.admin_username
+      }
+
+      env {
+        key = "ADMIN_PASSWORD"
+        value = var.admin_password
+      }
+
+      env {
+        key = "JWT_SECRET"
+        value = var.jwt_secret
+      }
+      
+      env {
+        key = "PAPERPOD_SCHEMA"
+        value = "authentication"
+        # FIXME make all secret
+      }
+
+      env {
+        key = "PGPORT"
+        value = "$${db.PORT}"
+      }
+      env {
+        key = "PGHOST"
+        value = "$${db.HOSTNAME}"
+      }
+
+      env {
+        key = "PGDATABASE"
+        value = "$${db.DATABASE}"
+      }
+
+      env {
+        key = "PGUSER"
+        value = "$${db.USERNAME}"
+      }
+
+      env {
+        key = "PGPASSWORD"
+        value = "$${db.PASSWORD}"
+      }
+
+      env {
+        key = "DATABASE_CA"
+        value = "$${db.CA_CERT}"
+      }
+
+      env {
+        key = "STRIPE_API_KEY"
+        value = var.stripe_api_key
+      }
+
+      env { 
+        key = "LOG_LEVEL"
+        value = var.log_level
+      }
+
+      alert {
+        value    = 75
+        operator = "GREATER_THAN"
+        window   = "TEN_MINUTES"
+        rule     = "CPU_UTILIZATION"
+      }
+    }
+  }
 }
--- scripts/install-docker-machine.sh
+++
@@ -1,2 +0,0 @@
-curl -L https://github.com/docker/machine/releases/download/v0.8.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine
-sudo chmod +x /usr/local/bin/docker-machine

73f81d95c5ef935e6f5f14bc683b9c92ca8e8079
olaven <olav@sundfoer.com>
2023-01-23 20:10:41 +0100

    Revert "Attempting to end article tests manually"
    This reverts commit 6e2e9064747976bf5d8d204d8de888efb53ee28e.

--- packages/api/src/routes/article-routes.test.ts
+++ packages/api/src/routes/article-routes.test.ts
@@ -9,28 +9,21 @@ import { setupMigrations } from "../test-utils/test-utils";
 
 describe("The api for articles", () => {
   setupMigrations();
-  const post = (
-    token: string,
-    payload = test.mocks.articlePayload(),
-    done: jest.DoneCallback
-  ) =>
+  const post = (token: string, payload = test.mocks.articlePayload()) =>
     supertest(app)
       .post("/articles")
       .set("Authorization", "Bearer " + token)
-      .send(payload)
-      .end(done);
+      .send(payload);
 
-  const get = (token: string, done: jest.DoneCallback) =>
+  const get = (token: string) =>
     supertest(app)
       .get("/articles")
-      .set("Authorization", "Bearer " + token)
-      .end(done);
+      .set("Authorization", "Bearer " + token);
 
-  const del = (token: string, _id: string, done: jest.DoneCallback) =>
+  const del = (token: string, _id: string) =>
     supertest(app)
       .delete(`/articles/${_id}`)
-      .set("Authorization", "Bearer " + token)
-      .end(done);
+      .set("Authorization", "Bearer " + token);
 
   describe("the POST-endpoint for articles", () => {
     jest.mock("@paperpod/converter", () => ({
@@ -39,34 +32,34 @@ describe("The api for articles", () => {
       },
     }));
 
-    it("Does respond with something other than NOT_IMPLEMENTED", async (done) => {
-      const { status } = await supertest(app).post("/articles").end(done);
+    it("Does respond with something other than NOT_IMPLEMENTED", async () => {
+      const { status } = await supertest(app).post("/articles");
       expect(status).not.toEqual(kall.NOT_IMPLEMENTED);
     });
 
-    it("Does respond with UNAUTHORIZED if no token is passed", async (done) => {
-      const { status } = await post(null, test.mocks.articlePayload(), done);
+    it("Does respond with UNAUTHORIZED if no token is passed", async () => {
+      const { status } = await post(null);
       expect(status).toEqual(kall.UNAUTHORIZED);
     });
 
-    it("Does respond with FORBIDDEN if user does not have an active subscription", async (done) => {
+    it("Does respond with FORBIDDEN if user does not have an active subscription", async () => {
       const token = jwt.sign({
         ...test.mocks.user(),
         subscription: "inactive",
       });
-      const { status } = await post(token, test.mocks.articlePayload(), done);
+      const { status } = await post(token);
       expect(status).toEqual(kall.FORBIDDEN);
     });
 
-    it("Does respond with CREATED if a valid request is made", async (done) => {
+    it("Does respond with CREATED if a valid request is made", async () => {
       const token = jwt.sign(test.mocks.user());
-      const { status } = await post(token, test.mocks.articlePayload(), done);
+      const { status } = await post(token);
       expect(status).toEqual(kall.CREATED);
     });
 
-    it("Does return an article on valid request", async (done) => {
+    it("Does return an article on valid request", async () => {
       const token = jwt.sign(test.mocks.user());
-      const { body } = await post(token, test.mocks.articlePayload(), done);
+      const { body } = await post(token);
 
       expect(body.id).toBeDefined();
       expect(body.original_url).toBeDefined();
@@ -74,14 +67,14 @@ describe("The api for articles", () => {
       expect(body.owner_id).toBeDefined();
     });
 
-    it("Does return the new article after creation", async (done) => {
+    it("Does return the new article after creation", async () => {
       const token = jwt.sign(test.mocks.user());
-      const before = (await (await get(token, done)).body) as models.Article[];
+      const before = (await (await get(token)).body) as models.Article[];
 
       const payload = test.mocks.articlePayload();
-      await post(token, payload, done);
+      await post(token, payload);
 
-      const after = (await (await get(token, done)).body) as models.Article[];
+      const after = (await (await get(token)).body) as models.Article[];
 
       const inBefore = before.find(
         (article) => article.original_url === payload.link
@@ -94,85 +87,77 @@ describe("The api for articles", () => {
       expect(inAfter).toBeTruthy();
     });
 
-    it("Does not accept an article if it's not containting a valid link", async (done) => {
+    it("Does not accept an article if it's not containting a valid link", async () => {
       const token = jwt.sign(test.mocks.user());
-      post(
-        token,
-        {
-          link: "not-a-url",
-        },
-        done
-      ).expect(kall.BAD_REQUEST);
+      post(token, {
+        link: "not-a-url",
+      }).expect(kall.BAD_REQUEST);
     });
 
-    it("Does not respond with NOT_IMPLEMENTED if link leads to a pdf", async (done) => {
+    it("Does not respond with NOT_IMPLEMENTED if link leads to a pdf", async () => {
       const token = jwt.sign(test.mocks.user());
-      const { status } = await post(
-        token,
-        {
-          link: "https://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf",
-        },
-        done
-      );
+      const { status } = await post(token, {
+        link: "https://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf",
+      });
 
       expect(status).not.toBe(kall.NOT_IMPLEMENTED);
     });
   });
 
   describe("Retrieving articles", () => {
-    it("Does respond with UNAUTHORIZED if not logged in", async (done) => {
-      const { status } = await get(null, done);
+    it("Does respond with UNAUTHORIZED if not logged in", async () => {
+      const { status } = await get(null);
       expect(status).toEqual(kall.UNAUTHORIZED);
     });
 
-    it("responds with OK if user is logged in", async (done) => {
+    it("responds with OK if user is logged in", async () => {
       const token = jwt.sign(test.mocks.user());
-      const { status } = await get(token, done);
+      const { status } = await get(token);
       expect(status).toEqual(kall.OK);
     });
 
-    it("Does respond with FORBIDDEN if user does not have an active subscription", async (done) => {
+    it("Does respond with FORBIDDEN if user does not have an active subscription", async () => {
       const token = jwt.sign({
         ...test.mocks.user(),
         subscription: "inactive",
       });
-      const { status } = await get(token, done);
+      const { status } = await get(token);
       expect(status).toEqual(kall.FORBIDDEN);
     });
   });
 
   describe("The endpoint for deleting articles", () => {
-    it("Responds with UNAUTHORIZED if not logged in", async (done) => {
-      const { status } = await del(null, "some-article-id", done);
+    it("Responds with UNAUTHORIZED if not logged in", async () => {
+      const { status } = await del(null, "some-article-id");
       expect(status).toEqual(kall.UNAUTHORIZED);
     });
 
-    it("Does respond with FORBIDDEN if user does not have an active subscription", async (done) => {
+    it("Does respond with FORBIDDEN if user does not have an active subscription", async () => {
       const token = jwt.sign({
         ...test.mocks.user(),
         subscription: "inactive",
       });
       const article = await articles.persist(test.mocks.article());
-      const { status } = await del(token, article.id, done);
+      const { status } = await del(token, article.id);
       expect(status).toEqual(kall.FORBIDDEN);
     });
 
-    it("Responds with NOT_FOUND if the article does not exist", async (done) => {
+    it("Responds with NOT_FOUND if the article does not exist", async () => {
       const token = jwt.sign(test.mocks.user());
-      const { status } = await del(token, faker.datatype.uuid(), done);
+      const { status } = await del(token, faker.datatype.uuid());
 
       expect(status).toEqual(kall.NOT_FOUND);
     });
 
-    it("Responds with FORBIDDEN if the user does not own the article", async (done) => {
+    it("Responds with FORBIDDEN if the user does not own the article", async () => {
       const article = await articles.persist(test.mocks.article());
       const token = jwt.sign(test.mocks.user());
 
-      const { status } = await del(token, article.id, done);
+      const { status } = await del(token, article.id);
       expect(status).toEqual(kall.FORBIDDEN);
     });
 
-    it("Responds with NO_CONTENT if the user tries to delete their own article", async (done) => {
+    it("Responds with NO_CONTENT if the user tries to delete their own article", async () => {
       const user: models.User = {
         ...test.mocks.user(),
         subscription: "active",
@@ -184,11 +169,11 @@ describe("The api for articles", () => {
 
       const token = jwt.sign(user);
 
-      const { status } = await del(token, article.id, done);
+      const { status } = await del(token, article.id);
       expect(status).toEqual(kall.NO_CONTENT);
     });
 
-    it("Does actually delete article on valid request", async (done) => {
+    it("Does actually delete article on valid request", async () => {
       const user: models.User = {
         ...test.mocks.user(),
         subscription: "active",
@@ -200,7 +185,7 @@ describe("The api for articles", () => {
 
       const token = jwt.sign(user);
 
-      await del(token, article.id, done);
+      await del(token, article.id);
 
       const after = await articles.getById(article.id);
       expect(after).toEqual(null);
